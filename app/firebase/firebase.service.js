ca325cefcfee97a1cd75e16fd780ac9f0e096d45
Aaron Myatt <aaronmyatt@gmail.com>
2016-07-17 14:01:31 +0800

    move firebase content into subdir

--- app/firebase/firebase.service.js
+++ app/firebase/firebase.service.js
@@ -8,13 +8,14 @@
     firebaseDataService.$inject = ['FIREBASE_URL'];
 
     function firebaseDataService(FIREBASE_URL) {
-        var root = new Firebase(FIREBASE_URL);
+
+        var root = Firebase.database().ref();
 
         var service = {
             root: root,
             users: root.child('users'),
-            emails: root.child('emails'),
-            textMessages: root.child('textMessages')
+            // emails: root.child('emails'),
+            // textMessages: root.child('textMessages')
         };
 
         return service;
