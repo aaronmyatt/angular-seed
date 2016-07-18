2efa4953df2230c5d2676221cd3c92746789911d
Aaron Myatt <aaronmyatt@gmail.com>
2016-07-18 21:07:19 +0800

    update firebase dependencies

--- app/firebase/firebase.service.js
+++ app/firebase/firebase.service.js
@@ -5,17 +5,15 @@
         .module('app')
         .factory('firebaseDataService', firebaseDataService);
 
-    firebaseDataService.$inject = ['FIREBASE_URL'];
+    firebaseDataService.$inject = [];
 
-    function firebaseDataService(FIREBASE_URL) {
+    function firebaseDataService() {
 
-        var root = Firebase.database().ref();
+        var root = firebase.database().ref();
 
         var service = {
             root: root,
-            users: root.child('users'),
-            // emails: root.child('emails'),
-            // textMessages: root.child('textMessages')
+            users: root.child('users')
         };
 
         return service;
