346d66b50d88ddf76a6c383559c2946ea8d2382d
Aaron Myatt <aaronmyatt@gmail.com>
2016-07-28 23:37:25 +0800

    brick directive handles promise resolution

--- app/memory-brick/memory-brick.directive.js
+++ app/memory-brick/memory-brick.directive.js
@@ -9,69 +9,48 @@
         return {
             templateUrl: 'memory-brick/memory-brick.html',
             restrict: 'E',
+            replace: true,
             controller: Controller,
             controllerAs: 'vm',
             bindToController: true,
-            // scope: {
-            //     error: '=',
-            //     formTitle: '@',
-            //     submitAction: '&'
-            // }
-        };
-    }
-
-    Controller.$inject = ["$scope", "$mdDialog", "$mdMedia"];
-    function Controller($scope, $mdDialog, $mdMedia) {
-        var vm = this;
-        vm.status = '  ';
-        vm.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
-
-        vm.showAdvanced = function(ev) {
-            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && vm.customFullscreen;
-
-            $mdDialog.show({
-                controller: DialogController,
-                controllerAs: 'vm',
-                templateUrl: 'add-memory/modal.html',
-                parent: angular.element(document.body),
-                targetEvent: ev,
-                clickOutsideToClose: true,
-                fullscreen: useFullScreen
-            })
-                .then(function(answer) {
-                    vm.status = 'You said the information was "' + answer + '".';
-                }, function() {
-                    vm.status = 'You cancelled the dialog.';
-                });
-
-            $scope.$watch(function() {
-                return $mdMedia('xs') || $mdMedia('sm');
-            }, function(wantsFullScreen) {
-                vm.customFullscreen = (wantsFullScreen === true);
-            });
-        };
-    }
-
-    DialogController.$inject = ["$scope", "$mdDialog", "BrickService"];
-    function DialogController($scope, $mdDialog, BrickService) {
-        var vm = this;
-
-        vm.hide = function () {
-            $mdDialog.hide();
-        };
-        vm.cancel = function () {
-            $mdDialog.cancel();
-        };
-        vm.answer = function (answer) {
-            $mdDialog.hide(answer);
-        };
-
-        vm.submit = function() {
-            if ($scope.form.file.$valid && vm.file) {
-                console.log("Passing file to _BrickService_, ", vm.file);
-                BrickService.uploadBrick(vm.file);
+            scope: {
+                memory: '='
             }
         };
+    }
+
+    Controller.$inject = ["FirebaseStorageService", "$scope"];
+    function Controller(FirebaseStorageService, $scope) {
+        var vm = this;
+
+        vm.image = '';
+
+        function getImageUrl(memory){
+            FirebaseStorageService.getImageFromFileName(memory.file)
+                .then(success)
+                .catch(error);
+
+            function success(data){
+                console.log("_SUCCESS_", vm);
+                vm.memory.ref = data;
+                $scope.$apply();
+            }
+
+            function error(e){
+                console.log("_ERROR_", e);
+            }
+        }
+
+        function init(){
+            console.log('_gzMemoryBrickDirective_ init, ', vm);
+            getImageUrl(vm.memory);
+        }
+        init();
+    }
+
+    DialogController.$inject = [];
+    function DialogController() {
+        var vm = this;
     }
 
 })();
