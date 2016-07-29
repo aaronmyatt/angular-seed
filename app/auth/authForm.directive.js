0c07efa8995928db1302be6c090ca153420872e1
Aaron Myatt <aaronmyatt@gmail.com>
2016-07-29 17:40:53 +0800

    google auth provider login popup

--- app/auth/authForm.directive.js
+++ app/auth/authForm.directive.js
@@ -23,10 +23,28 @@
   function AuthFormController() {
     var vm = this;
 
+    vm.googleLogin = googleLogin;
+
     vm.user = {
       email: '',
       password: ''
     };
+
+    function googleLogin(){
+      firebase.auth().signInWithPopup(googleProvider).then(function(result) {
+        console.log("Google Login Success");
+        var token = result.credential.accessToken;
+        var user = result.user;
+        console.log("Google Login Success", user);
+      }).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log("Google Login Error", error);
+      });
+
+    }
   }
 
 })();
