285e61aab1fd4e92a92f5343d1514df5b168b098
Aaron Myatt <aaronmyatt@gmail.com>
2016-07-29 16:55:49 +0800

    move promise resolution out of service

--- app/firebase-storage-service/firebase-storage.service.js
+++ app/firebase-storage-service/firebase-storage.service.js
@@ -11,12 +11,18 @@
         var service = this;
 
         service.getImageFromFileName = getImageFromFileName;
+        service.saveFile = saveFile;
 
         return service;
 
         function getImageFromFileName(file){
             console.log("_getImageFromFileName_", file);
-            return storage.refFromURL('gs://remin-f1785.appspot.com/' + file).getDownloadURL();
+            return storage.refFromURL('gs://remin-f1785.appspot.com/images/' + file).getDownloadURL();
+        }
+
+        function saveFile(file){
+            console.log("Uploading file, ", file);
+            return storageRef.child('images/' + file.name).put(file);
         }
     }
 
